b291d3a5350087df0afaccd234ead077f8536e89
BrehCore <brehcore@gmail.com>
2023-11-16 17:56:33 -0300

    adding GameMinDTO, GameRepository, GameService and GameController

---
+++ dslist/src/main/java/com/coretechstudio/dslist/controllers/GameController.java
@@ -0,0 +1,26 @@
+package com.coretechstudio.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coretechstudio.dslist.entities.dto.GameMinDTO;
+import com.coretechstudio.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+	
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<GameMinDTO> findAll() {
+		List<GameMinDTO> result = gameService.findAll();
+		return result;
+	}		
+
+}
